24b9f217113648b67f84d7d98fec19c8dc6b0b41
LeidyYesi <cantamosaleluya@gmail.com>
2022-11-16 10:01:11 -0300

    primer commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-/* En esta primera etapa, necesitamos requerir tu módulo autos y guardarlo en la variable autosImportados que se encuentra en la misma carpeta del archivo donde estás trabajando. Recordá que es el mismo archivo que creaste en la etapa anterior. 
+/* Etapa 2
+En esta primera etapa, necesitamos requerir tu módulo autos y guardarlo en la variable autosImportados que se encuentra en la misma carpeta del archivo donde estás trabajando. Recordá que es el mismo archivo que creaste en la etapa anterior. 
 Además, necesitarás crear un objeto literal llamado concesionaria que contendrá todas las funcionalidades que el cliente solicita.
 Por último, nuestro objeto literal debe tener un atributo llamado autos que contenga la lista de automóviles importada anteriormente.
 */
@@ -8,7 +9,8 @@ let autosImportados = require("./autos.js");
 const concesionaria = {
   autos: autosImportados,
 
-  /*Ahora que la concesionaria tiene los autos, es posible crear la funcionalidad buscarAuto que reciba por parámetro la patente y devuelva el auto al cual le corresponde. En caso de no encontrar el mismo, deberá retornar null.
+  /*Etapa 3
+  Ahora que la concesionaria tiene los autos, es posible crear la funcionalidad buscarAuto que reciba por parámetro la patente y devuelva el auto al cual le corresponde. En caso de no encontrar el mismo, deberá retornar null.
    */
 
   buscarAuto: function (patente) {
@@ -18,7 +20,7 @@ const concesionaria = {
     return autoEncontrado == undefined ? null : autoEncontrado;
   },
 
-  /*
+  /* Etapa 4
 Ahora, María les pide que agreguen la funcionalidad de venderAuto que recibe la patente y, en caso de encontrar al automóvil, le asigna el estado de vendido.
 */
 
@@ -30,7 +32,7 @@ Ahora, María les pide que agreguen la funcionalidad de venderAuto que recibe la
     }
   },
 
-  /*
+  /*Etapa 5
 La primera es poder contar, como concesionaria, con la habilidad de poder tener la lista de autos para la venta. A lo cual, María, cree que es una tarea sencilla que Juan y vos pueden encarar solos, usando la función autosParaLaVenta, aunque por las dudas ella les recuerda que no deberían de aparecer los autos que ya fueron vendidos.
 */
 
@@ -40,7 +42,7 @@ La primera es poder contar, como concesionaria, con la habilidad de poder tener 
     });
   },
 
-  /*
+  /*Etapa 6
 Resulta que a la concesionaria le suelen preguntar muy seguido cuáles de los autos para la venta son 0 km. Tené en cuenta que María considera que un auto 0 km es aquel que tenga un kilometraje menor a 100. Vas a tener que desarrollar la funcionalidad autosNuevos.
 ¿Cómo podés resolver esto reutilizando la función autosParaLaVenta?
 */
@@ -52,21 +54,21 @@ Resulta que a la concesionaria le suelen preguntar muy seguido cuáles de los au
     return resultadoAutos;
   },
 
-  /*
+  /*Etapa 7
 El cliente le pidió saber cuánto dinero generaron las ventas.
 María te pide que completes la función listaDeVentas que devuelve una lista que contiene el precio de venta de cada auto vendido. A esto, Juan, que está al lado tuyo, se le escapa la frase "mmm.....estoy seguro que alguna función de arrays nos va a servir, pero no me acuerdo".
 */
 
-listaDeVentas: function(){
-  let vendidos = this.autos.filter(function(auto){
-     return auto.vendido
-  })
-  return vendidos.map(function(auto){
-     return auto.precio
-  })
-},
+  listaDeVentas: function () {
+    let vendidos = this.autos.filter(function (auto) {
+      return auto.vendido;
+    });
+    return vendidos.map(function (auto) {
+      return auto.precio;
+    });
+  },
 
-/*Etapa 8
+  /*Etapa 8
 Terminada esta función, María te pide que resuelvas la funcionalidad de totalDeVentas, que justamente nos devuelva la sumatoria del valor de todas las ventas realizadas. Acá el único requerimiento técnico explícito es que utilices la función reduce, ¡a codear!
 */
 
@@ -96,12 +98,14 @@ capacidadDePagoTotal: 100000
 }
 */
 
-puedeComprar: function (auto, persona) {
-  return  (auto.precio <= persona.capacidadDePagoTotal && auto.precio / auto.cuotas <= persona.capacidadDePagoEnCuotas);
-},
- 
+  puedeComprar: function (auto, persona) {
+    return (
+      auto.precio <= persona.capacidadDePagoTotal &&
+      auto.precio / auto.cuotas <= persona.capacidadDePagoEnCuotas
+    );
+  },
 
-/*Etapa 10
+  /*Etapa 10
 Hay que escribir la función autosQuePuedeComprar, que recibe una persona y devuelve la lista de autos que puede comprar.
 La función debe de realizar los siguientes pasos:
   1) Obtener los autos para la venta
@@ -109,12 +113,12 @@ La función debe de realizar los siguientes pasos:
   3) Por cada uno de los autos debe de probar si la persona puede comprarlo, ¿ya hay alguna funcionalidad que me permita hacer esto?.
 */
 
-autosQuePuedeComprar: function (persona) {
-    let autosDiponibles = this.autosParaLaVenta().filter(auto => {
+  autosQuePuedeComprar: function (persona) {
+    let autosDiponibles = this.autosParaLaVenta().filter((auto) => {
       return this.puedeComprar(auto, persona);
-    })
+    });
     return autosDiponibles;
-  }
+  },
 };
 
 //console.log(autosImportados);
---
+++ readme.md
@@ -0,0 +1,22 @@
+Vamos a simular una situación de trabajo para situarnos en un contexto. Estás trabajando como desarrollador de CodeAR S.A., una reconocida software factory. En el equipo de trabajo contás con María y con Juan.
+
+Maria:
+Es líder técnica del área. Es una persona muy ocupada y su rol consiste en traducir las necesidades del cliente a requerimientos técnicos.
+
+Juan:
+Es pasante y está empezando en el mundo de la programación. Si bien es algo tímido, le gusta aprender cosas nuevas y encarar nuevos desafíos.
+
+Vos:
+Si bien estás aprendiendo cosas como Juan, ya contás con algunos conocimientos previos.
+
+María presenta al equipo un nuevo proyecto para una concesionaria de automóviles, cuya principal línea de negocios es la compra y venta de automóviles. La concesionaria necesita construir una lista con todos los vehículos que tiene registrados. Por cada vehículo necesita conocer la siguiente información:
+
+Marca (Ford, Fiat, etc). (marca)
+Modelo (Fiesta, Corolla, etc). (modelo)
+Color (Azul, Rojo, etc). (color)
+Año de fabricación. (año)
+Cantidad de kilómetros recorridos. (km)
+Precio final. (precio)
+Cantidad de cuotas. (cuotas)
+Patente. (patente)
+Vendido: para indicar si el auto está o no vendido. (vendido)
